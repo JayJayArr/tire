b778f83fd6230ef971e4e5f9772fde69db317323
JayJayArr <JayArr@outlook.de>
2024-11-13 12:20:59 +0100

    add theme

--- tire/src/app/app.config.ts
+++ tire/src/app/app.config.ts
@@ -1,8 +1,20 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import {
+  ApplicationConfig,
+  importProvidersFrom,
+  provideZoneChangeDetection,
+} from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { NbThemeModule } from '@nebular/theme';
+import { NbEvaIconsModule } from '@nebular/eva-icons';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [
+    provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    importProvidersFrom(NbThemeModule.forRoot({ name: 'dark' })),
+    importProvidersFrom(NbEvaIconsModule),
+    importProvidersFrom(BrowserAnimationsModule),
+  ],
 };
