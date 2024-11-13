2791ef791973bbd71728af2d2ff186f21fac0ab8
JayJayArr <JayArr@outlook.de>
2024-11-13 12:50:55 +0100

    add admin, login, personal, settings component

--- tire/src/app/app.routes.ts
+++ tire/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
+import { PersonalComponent } from './personal/personal.component';
+import { AdminComponent } from './admin/admin.component';
+import { SettingsComponent } from './settings/settings.component';
+import { LoginComponent } from './login/login.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', redirectTo: 'personal', pathMatch: 'full' },
+  { path: 'personal', component: PersonalComponent },
+  { path: 'admin', component: AdminComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'settings', component: SettingsComponent },
+];
